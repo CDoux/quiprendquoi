5553d9d48aa33205f799f761bb5d6ab52a08f870
CDoux <cassidoux@gmail.com>
2020-03-17 10:00:50 +0100

    Fin partie 06

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+require("dotenv").config();
 
 app.get('/', function(req, res) {
-  res.send('Hey');
+  res.render('index');
 });
 
-app.listen(port, () => console.log(`Front app listening on port ${port}!`));
+app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
+app.set('view engine', 'pug');
