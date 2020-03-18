16cd0008be0cd0c332c23e0fb01e5af2ce381697
CDoux <cassidoux@gmail.com>
2020-03-18 08:19:57 +0100

    partie 9

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 require("dotenv").config();
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', { title: 'Qui prend quoi ?' });
+});
+
+app.get('/party', function(req, res) {
+  res.send('Post ok !')
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
